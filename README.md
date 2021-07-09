a4990a3e9224e3ba8561b7872490ea59af2fb3f4
HuanzhuoWu <huanzhuo.wu@tu-dresden.de>
2021-07-09 15:52:16 +0200

    add source codes

---
+++ README.md
@@ -0,0 +1,39 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+# Progressive ICA (pICA)
+
+## Table of Contents
+- [Progressive ICA (pICA)](#progressive-ica-pica)
+  - [Table of Contents](#table-of-contents)
+  - [Description](#description)
+  - [Requirements](#requirements)
+  - [Citation](#citation)
+  - [About Us](#about-us)
+  - [License](#license)
+  - [Todo](#todo)
+
+
+## Description
+
+To be done.
+
+## Requirements
+
+To be done
+
+## Citation
+
+## About Us
+
+We are researchers at the Deutsche Telekom Chair of Communication Networks (ComNets) at TU Dresden, Germany. Our focus is on in-network computing.
+
+* **Huanzhuo Wu** - huanzhuo.wu@tu-dresden.de or wuhuanzhuo@gmail.com
+* **Yunbin Shen** - yunbin.shen@mailbox.tu-dresden.de or shenyunbin@outlook.com
+
+## License
+
+This project is licensed under the [MIT license](./LICENSE).
+
+## Todo
+1. Update file path.
+2. Add test data set.
--- picalite.py
+++ picalite.py
@@ -1,12 +1,9 @@
 import numpy as np
 import warnings
-# 作者: 沈云彬
-# 日期: 2021年5月3日
 
 
 class ProgressiveICALite():
 
-    # Pica 改进后的矩阵白化, 多了一个V的逆矩阵, 加快后面计算
     def _whiten_with_inv_v(self, X):
         X -= X.mean(axis=-1)[:, np.newaxis]
         A = np.dot(X, X.T)
@@ -19,7 +16,6 @@ class ProgressiveICALite():
         X1 = np.sqrt(X.shape[1]) * np.dot(V, X)
         return X1, V, V_inv
 
-    # 来自于Fastica的tanh函数
     def _logcosh(self, x, alpha=1.0):
         x *= alpha
         gx = np.tanh(x, x)
@@ -37,13 +33,10 @@ class ProgressiveICALite():
     def _cube(self, x):
         return x ** 3, (3 * x ** 2).sum(axis=-1)
 
-    # 来自于FastICA的去相关化
     def _sym_decorrelation(self, W):
         S, U = np.linalg.eigh(np.dot(W, W.T))
         np.clip(S, 1e-15, None, out=S)
         return np.linalg.multi_dot([U * (1. / np.sqrt(S)), U.T, W])
-
-    # Pica 带有梯度下降速率判断的牛顿迭代
 
     def _ica_par(self, X, W, grad_var_tol, tol, g, max_iter):
         lim_sum = 0
@@ -65,14 +58,12 @@ class ProgressiveICALite():
                 'PICA did not converge. Consider increasing tolerance or the maximum number of iterations.')
         return W, lim
 
-    # Pica 主要部分
     def _pica(self, X, proc_mode, init_ext_interval, dynamic_adj_coef, tol, grad_var_tol, g, max_iter):
         n, m = X.shape
         W = np.random.random_sample((n, n))
         ext_interval = int(init_ext_interval)
         ext_interval_divisor = dynamic_adj_coef
         while(True):
-            # Pica 的收尾模式选择, 有快速和普通模式两种选择, 快速模式根据计算中间结果判断是否计算部分数据就提前跳出, 普通模式会将全部数据迭代一遍
             if ext_interval < ext_interval_divisor:
                 if proc_mode == 'fast':
                     ext_interval = ext_interval_divisor // dynamic_adj_coef
@@ -84,22 +75,14 @@ class ProgressiveICALite():
                 else:
                     raise NameError(
                         'The value of proc_mode is invalid, it must be "fast" , "normal" or "precise". ')
-            # Pica 按顺序提取数据
             _X = X[:, :int(m // ext_interval)]
-            # Pica 按等间隔提取数据
             # _X = X[:, ::int(ext_interval)]
-            # Pica 重新白化并计算白化矩阵V的逆矩阵
             _X, V, V_inv = self._whiten_with_inv_v(_X)
-            # 计算适重新应白化后的X的W矩阵
             W = self._sym_decorrelation(np.dot(W, V_inv))
-            # Pica 根据梯度下降和 tol 判断是否该跳出当前 Extraction level 的牛顿迭代
             W, lim = self._ica_par(_X, W, grad_var_tol, tol, g, max_iter)
-            # 计算适用于未白化前的X的W矩阵
             W = np.dot(W, V)
-            # 判断是否计算完成并跳出
             if ext_interval < ext_interval_divisor:
                 break
-            # 根据梯度下降速率跳出法跳出模式最终计算出的lim 值和 tol比较, 如果 lim 小于 tol 则说明当前的 W 对于当前 Extraction 的数据已经过于收敛, 换句话说再计算下去, W 也不会改变太多, 反而会浪费大量时间, 这时候需要动态增加下次计算的数据量来增加 W 的收敛速度
             if lim < tol:
                 ext_interval_divisor *= dynamic_adj_coef
             else:
